bc1dfc2a9bf044b5878ddb98079c6411a511dd16
Anshul <anshulkhare7@gmail.com>
2018-08-26 11:43:57 +0530

    added shuffle feature

--- js/main.js
+++ js/main.js
@@ -90,4 +90,38 @@
 		}
 	});
 
+	///////////////////////////
+	// Custom
+    $("#btn-shuffle").bind('click', shuffle);        
+
+    function shuffle(){		
+        $("#idea-containter").each(function(){
+            var divs = $(this).find('.ideas');
+            for(var i = 0; i < divs.length; i++) 
+				$(divs[i]).remove();            
+            //the fisher yates algorithm, from http://stackoverflow.com/questions/2450954/how-to-randomize-a-javascript-array
+            var i = divs.length;
+            if ( i == 0 ) 
+				return false;
+            
+			while ( --i ) {
+               var j = Math.floor( Math.random() * ( i + 1 ) );
+               var tempi = divs[i];
+               var tempj = divs[j];
+               divs[i] = tempj;
+               divs[j] = tempi;
+             }
+            
+			for(var i = 0; i < divs.length; i++) 
+				$(divs[i]).appendTo(this);
+        });                    
+    }
+
+	// $('#btn-shuffle').click(function(){
+    //     var ideaslength = $('.ideas').length;
+	// 	var idea = $('.ideas').first();
+	// 	// alert(idea.html());
+	// 	// $('#idea-container').a;
+    // });
+
 })(jQuery);
