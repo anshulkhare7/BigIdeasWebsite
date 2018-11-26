6728798fab997e3c6c9941b03bb726c0b9ec1771
Anshul <anshulkhare7@gmail.com>
2018-11-27 00:00:06 +0530

    customized for SN. Image optimization. Css changes.

--- js/image-popup.js
+++ js/image-popup.js
@@ -2,14 +2,15 @@ $(function () {
     "use strict";
     
     $(".card__info").click(function () {
-        var $src = $(this).children().filter("img").attr("src");
+        if($(window).width() <= 767){
+            return;
+        }
+        var $src = $(this).find("#popup-image-src").html();
         $(".popup-show").fadeIn();
         $(".img-show img").attr("src", $src);
     });
     
     $("span, .overlay").click(function () {
         $(".popup-show").fadeOut();
-    });
-    
-    
+    });    
 });
